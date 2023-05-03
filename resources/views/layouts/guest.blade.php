191357c3a3c4e34f2a3e3a98198c171544df0ad5
Muhammad Khaidir <khaedir3018@gmail.com>
2023-05-04 00:13:01 +0800

    ganti logo

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -27,7 +27,7 @@
                         <div class="shrink-0 mr-4">
                             <!-- Logo -->
                             <a class="block group" href="{{ route('home') }}" aria-label="Cruip">
-                                <img src="{{ asset('images/logo-gambar.png') }}" alt="" class="h-12">
+                                <img src="{{ asset('images/logo_sekolah.png') }}" alt="" class="h-12">
                             </a>
                         </div>
 
